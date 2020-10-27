982131885a2ea0e310a2bd060fd316f808f1ab23
Valentina Ponamareva <valiazin@gmail.com>
2020-10-27 21:42:42 +0300

    feat: implement dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false;
+  
+  let dreamTeam = '';
+
+  for (let name of members) {
+    if (typeof name === 'string') {
+      name = name.trim();
+      dreamTeam += name[0];
+      console.log(dreamTeam);
+    }
+  }
+  
+  let sortedDreamTeam = dreamTeam.toUpperCase().split('').sort().join('');
+  return sortedDreamTeam;
 };

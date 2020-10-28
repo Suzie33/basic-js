2994576a5466f94952174532d8ce3cdf25b636ea
Valentina Ponamareva <valiazin@gmail.com>
2020-10-28 14:09:02 +0300

    feat: add transform array silution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,49 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) throw new Error('error');
+
+  let transformedArr = arr.slice();
+  let deleted = {};
+
+  for (let i = 0; i < transformedArr.length; i++) {
+    if (transformedArr[i] === '--discard-next') {
+      if (i === transformedArr.length - 1) {
+        transformedArr.splice(i, 1, deleted);
+      } else {
+        transformedArr.splice(i, 2, deleted);
+      }
+    }
+    if (transformedArr[i] === '--discard-prev') {
+      if (i === 0) {
+        transformedArr.splice(0, 1, deleted);
+      } else {
+        transformedArr.splice(i-1, 2, deleted);
+      };
+    }
+    if (transformedArr[i] === '--double-next') {
+      if (i === transformedArr.length - 1) {
+        transformedArr.splice(i, 1);
+      } else {
+        transformedArr.splice(i, 1, transformedArr[i + 1]);
+      }
+    }
+    if (transformedArr[i] === '--double-prev') {
+      if (i === 0) {
+        transformedArr.splice(i, 1);
+      } else if (transformedArr[i - 1] === deleted) {
+        transformedArr.splice(i, 1);
+      } else {
+        transformedArr.splice(i, 1, transformedArr[i - 1]);
+      }
+    }
+  }
+
+  let filteredArray = transformedArr.filter((item) => {
+    return item !== deleted;
+  })
+  // console.log(filteredArray);
+  return filteredArray;
 };
+
+// module.exports(['--discard-next','DEF','--discard-prev',8.963,NaN,'--double-next']);

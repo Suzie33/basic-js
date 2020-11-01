de71904e482cbf68eb5034f153fa02a13e5ec78a
Valentina Ponamareva <valiazin@gmail.com>
2020-11-01 17:00:58 +0300

    feat: add vigenere-cipher solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,58 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor (mode = true) {
+    this.mode = mode;
+  }
+  encrypt(message, key) {
+    if (message === undefined || key === undefined) {
+      throw new Error('error');
+    }
+
+    const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let encryptWord = "";
+    let keyIndConter = 0;
+
+    for (let i = 0; i < message.length; i++) {
+      let charIndInAlf = alphabet.indexOf(message[i].toUpperCase());
+      let keyIndInAlf = alphabet.indexOf(key.charAt(keyIndConter % key.length).toUpperCase());
+
+      if (charIndInAlf !== -1) {
+        encryptWord += alphabet.charAt((charIndInAlf + keyIndInAlf) % alphabet.length);
+        keyIndConter++;
+      } else {
+        encryptWord += message[i];
+      }
+    }
+    if (!this.mode) {
+      encryptWord = encryptWord.split('').reverse().join('');
+    }
+    return encryptWord;
+  }
+  decrypt(encryptedMessage, key) {
+    if (encryptedMessage === undefined || key === undefined) {
+      throw new Error('error');
+    }
+
+    const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let decryptWord = "";
+    let keyIndConter = 0;
+
+    for (let i = 0; i < encryptedMessage.length; i++) {
+      let charIndInAlf = alphabet.indexOf(encryptedMessage[i].toUpperCase());
+      let keyIndInAlf = alphabet.indexOf(key.charAt(keyIndConter % key.length).toUpperCase());
+
+      if (charIndInAlf !== -1) {
+        decryptWord += alphabet.charAt(((charIndInAlf + alphabet.length) - keyIndInAlf) % alphabet.length);
+        keyIndConter++;
+      } else {
+        decryptWord += encryptedMessage[i];
+      }
+    }
+    if (!this.mode) {
+      decryptWord = decryptWord.split('').reverse().join('');
+    }
+    return decryptWord;
   }
 }
 
